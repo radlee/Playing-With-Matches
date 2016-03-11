141a5aee2e3a8422b42c01d511c1d56a6e9147f3
radlee <lolito@projectcodex.co>
2016-03-11 17:36:41 +0200

    Playing-With-Matches(for Loop*)

--- Playing/script_parent.js
+++ Playing/script_parent.js
@@ -8,101 +8,182 @@ for (var i = 0; i < numbers.length; i++) {
     var number = numbers[i];
     number.innerHTML = random(3);
 }
-//checking for matched
-if (numbers[0].innerHTML === numbers[1].innerHTML){
 
-    numbers[0].classList.add("highlight");
-    numbers[1].classList.add("highlight");
-    document.getElementById("parent").classList.add("highlightParent")
-}
-//The pattern is that you add 3's here
-if (numbers[1].innerHTML === numbers[2].innerHTML){
+//Row Matches function
+var rowMatch = function(base){
+  var matchCounter =0;
+  if (numbers[base].innerHTML === numbers[base + 2].innerHTML){
+      numbers[base].classList.add("highlight");
+      numbers[base + 2].classList.add("highlight");
+      document.getElementById("parent").classList.add("highlightParent")
+      matchCounter++;
+  }
 
-    numbers[1].classList.add("highlight");
-    numbers[2].classList.add("highlight");
-}
-if (numbers[0].innerHTML === numbers[2].innerHTML){
-    numbers[0].classList.add("highlight");
-    numbers[2].classList.add("highlight");
-}
-//Row2 ----
-if (numbers[3].innerHTML === numbers[4].innerHTML){
+  if (numbers[base].innerHTML === numbers[base + 1].innerHTML){
+      numbers[base].classList.add("highlight");
+      numbers[base + 1].classList.add("highlight");
+      document.getElementById("parent").classList.add("highlightParent1");
+      matchCounter++;
+  }
 
-    numbers[3].classList.add("highlight");
-    numbers[4].classList.add("highlight");
-    document.getElementById("parent").classList.add("highlightParent1")
-}
-if (numbers[4].innerHTML === numbers[5].innerHTML){ //  (1+3) = 4, (2+3) = 5
+  if (numbers[base + 1].innerHTML === numbers[base + 2].innerHTML){
+      numbers[base + 1].classList.add("highlight");
+      numbers[base + 2].classList.add("highlight");
+      document.getElementById("parent").classList.add("highlightParent2");
+      matchCounter++;
+  }
 
-    numbers[4].classList.add("highlight");
-    numbers[5].classList.add("highlight");
-}
-if (numbers[3].innerHTML === numbers[5].innerHTML){
-    numbers[3].classList.add("highlight");
-    numbers[5].classList.add("highlight");
-}
-//Row3 ----
-if (numbers[6].innerHTML === numbers[7].innerHTML){
+  return matchCounter;
 
-    numbers[6].classList.add("highlight");
-    numbers[7].classList.add("highlight");
-    document.getElementById("parent").classList.add("highlightParent2")
-}
-if (numbers[7].innerHTML === numbers[8].innerHTML){ //  +3
+};
+//Collumn Match function
 
-    numbers[7].classList.add("highlight");
-    numbers[8].classList.add("highlight");
-}
-if (numbers[6].innerHTML === numbers[8].innerHTML){
-    numbers[6].classList.add("highlight");
-    numbers[8].classList.add("highlight");
-}
-//Collumn ----
-if (numbers[0].innerHTML === numbers[3].innerHTML){
+var colMatch = function(base){
+  var countCols =0;
+  if (numbers[base].innerHTML === numbers[ base + 3].innerHTML){
 
-    numbers[0].classList.add("highlight");
-    numbers[3].classList.add("highlight");
-}
-if (numbers[0].innerHTML === numbers[6].innerHTML){ //  +3
+      numbers[base].classList.add("highlight");
+      numbers[base + 3].classList.add("highlight");
+  }
+  if (numbers[base].innerHTML === numbers[base + 6].innerHTML){ //  +3
 
-    numbers[0].classList.add("highlight");
-    numbers[6].classList.add("highlight");
-}
-if (numbers[3].innerHTML === numbers[6].innerHTML){
-    numbers[3].classList.add("highlight");
-    numbers[6].classList.add("highlight");
-}
-//Collumn 2 ----
-if (numbers[1].innerHTML === numbers[4].innerHTML){
+      numbers[base].classList.add("highlight");
+      numbers[base+ 6].classList.add("highlight");
+  }
+  if (numbers[base + 3].innerHTML === numbers[base + 6].innerHTML){
+      numbers[base + 3].classList.add("highlight");
+      numbers[base + 6].classList.add("highlight");
+  }
+  countCols++;
 
-    numbers[1].classList.add("highlight");
-    numbers[4].classList.add("highlight");
-}
-if (numbers[1].innerHTML === numbers[7].innerHTML){ //  +3
+};
 
-    numbers[1].classList.add("highlight");
-    numbers[7].classList.add("highlight");
+//highlighting the diagonalMatch  here and it is not working!!!!!!
+if(numbers[0].innerHTML === numbers[4]){
+  numbers[0].classList.add("highlight");
+  numbers[4].classList.add("highlight");
 }
-if (numbers[4].innerHTML === numbers[7].innerHTML){
-    numbers[4].classList.add("highlight");
-    numbers[7].classList.add("highlight");
+if(numbers[4].innerHTML === numbers[8]){
+  numbers[4].classList.add("highlight");
+  numbers[8].classList.add("highlight");
 }
-//Collumn 3 ----
-if (numbers[2].innerHTML === numbers[5].innerHTML){
-
-    numbers[2].classList.add("highlight");
-    numbers[5].classList.add("highlight");
-}
-if (numbers[2].innerHTML === numbers[8].innerHTML){ //  +3
-
-    numbers[2].classList.add("highlight");
-    numbers[8].classList.add("highlight");
-}
-if (numbers[5].innerHTML === numbers[8].innerHTML){
-    numbers[5].classList.add("highlight");
-    numbers[8].classList.add("highlight");
+if(numbers[0].innerHTML === numbers[8]){
+  numbers[0].classList.add("highlight");
+  numbers[8].classList.add("highlight");
 }
 
-// var match = function(start){
+
+
+// var diagonalMatch = function(base){
+//
+//
+// };
+// var diagonal2Match = function(base){
+//
+//   if(numbers[base + 2].innerHTML === numbers[base+ 4]){
+//     numbers[base + 2].classList.add("highlightDia2");
+//     numbers[base + 4].classList.add("highlightDia2");
+//   }
+//
+//
+//   if(numbers[base + 2].innerHTML === numbers[base + 6]){
+//     numbers[base + 2].classList.add("highlightDia2");
+//     numbers[base + 6].classList.add("highlightDia2");
+//   }
+//
+// };
+
+
+var matches = colMatch(0);
+var matches = colMatch(1);
+var matches = colMatch(2);
+
+var matches = rowMatch(0);
+var matches = rowMatch(3);
+var matches = rowMatch(6);
+
+// var matches = diagonalMatch(0);
+// var matches = diagonal2Match(2);
+//
+
+// //Row2 ----
+// if ((numbers[3].innerHTML === numbers[4].innerHTML) && (numbers[4].innerHTML === numbers[5].innerHTML)){
+//
+//     numbers[3].classList.add("highlight");
+//     numbers[4].classList.add("highlight5");
 //
 // }
+// if (numbers[4].innerHTML === numbers[5].innerHTML){ //  (1+3) = 4, (2+3) = 5
+//
+//     numbers[4].classList.add("highlight");
+//     numbers[5].classList.add("highlight");
+// }
+// if (numbers[3].innerHTML === numbers[5].innerHTML){
+//     numbers[3].classList.add("highlight");
+//     numbers[5].classList.add("highlight");
+// }
+// //Row3 ----
+// if (numbers[6].innerHTML === numbers[7].innerHTML){
+//
+//     numbers[6].classList.add("highlight");
+//     numbers[7].classList.add("highlight");
+//     document.getElementById("parent").classList.add("highlightParent2")
+// }
+// if (numbers[7].innerHTML === numbers[8].innerHTML){ //  +3
+//
+//     numbers[7].classList.add("highlight");
+//     numbers[8].classList.add("highlight");
+// }
+// if (numbers[6].innerHTML === numbers[8].innerHTML){
+//     numbers[6].classList.add("highlight");
+//     numbers[8].classList.add("highlight");
+// }
+// //Collumn ----
+// if (numbers[0].innerHTML === numbers[3].innerHTML){
+//
+//     numbers[0].classList.add("highlight");
+//     numbers[3].classList.add("highlight");
+// }
+// if (numbers[0].innerHTML === numbers[6].innerHTML){ //  +3
+//
+//     numbers[0].classList.add("highlight");
+//     numbers[6].classList.add("highlight");
+// }
+// if (numbers[3].innerHTML === numbers[6].innerHTML){
+//     numbers[3].classList.add("highlight");
+//     numbers[6].classList.add("highlight");
+// }
+// //Collumn 2 ----
+// if ((numbers[1].innerHTML === numbers[4].innerHTML) && (numbers[1].innerHTML === numbers[7].innerHTML)){
+//
+//     numbers[1].classList.add("highlight");
+//     numbers[4].classList.add("highlight5");
+// }
+// if (numbers[1].innerHTML === numbers[7].innerHTML){ //  +3
+//
+//     numbers[1].classList.add("highlight");
+//     numbers[7].classList.add("highlight");
+// }
+// if (numbers[4].innerHTML === numbers[7].innerHTML){
+//     numbers[4].classList.add("highlight");
+//     numbers[7].classList.add("highlight");
+// }
+// //Collumn 3 ----
+// if (numbers[2].innerHTML === numbers[5].innerHTML){
+//
+//     numbers[2].classList.add("highlight");
+//     numbers[5].classList.add("highlight");
+// }
+// if (numbers[2].innerHTML === numbers[8].innerHTML){ //  +3
+//
+//     numbers[2].classList.add("highlight");
+//     numbers[8].classList.add("highlight");
+// }
+// if (numbers[5].innerHTML === numbers[8].innerHTML){
+//     numbers[5].classList.add("highlight");
+//     numbers[8].classList.add("highlight");
+// }
+//
+// // var match = function(start){
+// //
+// // }
