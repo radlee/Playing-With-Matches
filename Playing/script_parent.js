71577d0cb383d726c7bae6748f589336da369585
radlee <lolito@projectcodex.co>
2016-03-12 02:38:29 +0200

    Playing-With-Matches(A Different Parent)

--- Playing/script_parent.js
+++ Playing/script_parent.js
@@ -6,33 +6,37 @@ var numbers = document.querySelectorAll(".number");
 //loop through all the div's and allocate a random number
 for (var i = 0; i < numbers.length; i++) {
     var number = numbers[i];
-    number.innerHTML = random(3);
+    number.innerHTML = random(9);
 }
+
 
 //Row Matches function
 var rowMatch = function(base){
   var matchCounter =0;
   if (numbers[base].innerHTML === numbers[base + 2].innerHTML){
-      numbers[base].classList.add("highlight");
-      numbers[base + 2].classList.add("highlight");
-      document.getElementById("parent").classList.add("highlightParent")
+      numbers[base].classList.add("highlightRow");
+      numbers[base + 2].classList.add("highlightRow");
+      document.getElementById("parent").classList.add("highlightParent");
+      //document.getElementById("row1Match").classList.add("red");
       matchCounter++;
+      document.getElementById("rowTex").style.color = "green";
   }
 
   if (numbers[base].innerHTML === numbers[base + 1].innerHTML){
-      numbers[base].classList.add("highlight");
-      numbers[base + 1].classList.add("highlight");
+      numbers[base].classList.add("highlightRow");
+      numbers[base + 1].classList.add("highlightRow");
       document.getElementById("parent").classList.add("highlightParent1");
       matchCounter++;
+      document.getElementById("rowTex").style.color = "green";
   }
 
   if (numbers[base + 1].innerHTML === numbers[base + 2].innerHTML){
-      numbers[base + 1].classList.add("highlight");
-      numbers[base + 2].classList.add("highlight");
+      numbers[base + 1].classList.add("highlightRow");
+      numbers[base + 2].classList.add("highlightRow");
       document.getElementById("parent").classList.add("highlightParent2");
       matchCounter++;
+      document.getElementById("rowTex").style.color = "green";
   }
-
   return matchCounter;
 
 };
@@ -41,58 +45,27 @@ var rowMatch = function(base){
 var colMatch = function(base){
   var countCols =0;
   if (numbers[base].innerHTML === numbers[ base + 3].innerHTML){
+      numbers[base].classList.add("highlightCol");
+      numbers[base + 3].classList.add("highlightCol");
+      // document.getElementById("rowTex").style.color = "red";
+      countCols++;
 
-      numbers[base].classList.add("highlight");
-      numbers[base + 3].classList.add("highlight");
   }
   if (numbers[base].innerHTML === numbers[base + 6].innerHTML){ //  +3
-
-      numbers[base].classList.add("highlight");
-      numbers[base+ 6].classList.add("highlight");
+      numbers[base].classList.add("highlightCol");
+      numbers[base+ 6].classList.add("highlightCol");
+      countCols++;
+      // document.getElementById("colTex").style.color = #660033;;
   }
   if (numbers[base + 3].innerHTML === numbers[base + 6].innerHTML){
-      numbers[base + 3].classList.add("highlight");
-      numbers[base + 6].classList.add("highlight");
+      numbers[base + 3].classList.add("highlightCol");
+      numbers[base + 6].classList.add("highlightCol");
+      countCols++;
+      // document.getElementById("colTex").style.color = #660033;
   }
-  countCols++;
 
+  return countCols;
 };
-
-//highlighting the diagonalMatch  here and it is not working!!!!!!
-if(numbers[0].innerHTML === numbers[4]){
-  numbers[0].classList.add("highlight");
-  numbers[4].classList.add("highlight");
-}
-if(numbers[4].innerHTML === numbers[8]){
-  numbers[4].classList.add("highlight");
-  numbers[8].classList.add("highlight");
-}
-if(numbers[0].innerHTML === numbers[8]){
-  numbers[0].classList.add("highlight");
-  numbers[8].classList.add("highlight");
-}
-
-
-
-// var diagonalMatch = function(base){
-//
-//
-// };
-// var diagonal2Match = function(base){
-//
-//   if(numbers[base + 2].innerHTML === numbers[base+ 4]){
-//     numbers[base + 2].classList.add("highlightDia2");
-//     numbers[base + 4].classList.add("highlightDia2");
-//   }
-//
-//
-//   if(numbers[base + 2].innerHTML === numbers[base + 6]){
-//     numbers[base + 2].classList.add("highlightDia2");
-//     numbers[base + 6].classList.add("highlightDia2");
-//   }
-//
-// };
-
 
 var matches = colMatch(0);
 var matches = colMatch(1);
@@ -102,88 +75,63 @@ var matches = rowMatch(0);
 var matches = rowMatch(3);
 var matches = rowMatch(6);
 
-// var matches = diagonalMatch(0);
-// var matches = diagonal2Match(2);
-//
+//diagonalMatch---
 
-// //Row2 ----
-// if ((numbers[3].innerHTML === numbers[4].innerHTML) && (numbers[4].innerHTML === numbers[5].innerHTML)){
-//
-//     numbers[3].classList.add("highlight");
-//     numbers[4].classList.add("highlight5");
-//
-// }
-// if (numbers[4].innerHTML === numbers[5].innerHTML){ //  (1+3) = 4, (2+3) = 5
-//
-//     numbers[4].classList.add("highlight");
-//     numbers[5].classList.add("highlight");
-// }
-// if (numbers[3].innerHTML === numbers[5].innerHTML){
-//     numbers[3].classList.add("highlight");
-//     numbers[5].classList.add("highlight");
-// }
-// //Row3 ----
-// if (numbers[6].innerHTML === numbers[7].innerHTML){
-//
-//     numbers[6].classList.add("highlight");
-//     numbers[7].classList.add("highlight");
-//     document.getElementById("parent").classList.add("highlightParent2")
-// }
-// if (numbers[7].innerHTML === numbers[8].innerHTML){ //  +3
-//
-//     numbers[7].classList.add("highlight");
-//     numbers[8].classList.add("highlight");
-// }
-// if (numbers[6].innerHTML === numbers[8].innerHTML){
-//     numbers[6].classList.add("highlight");
-//     numbers[8].classList.add("highlight");
-// }
-// //Collumn ----
-// if (numbers[0].innerHTML === numbers[3].innerHTML){
-//
-//     numbers[0].classList.add("highlight");
-//     numbers[3].classList.add("highlight");
-// }
-// if (numbers[0].innerHTML === numbers[6].innerHTML){ //  +3
-//
-//     numbers[0].classList.add("highlight");
-//     numbers[6].classList.add("highlight");
-// }
-// if (numbers[3].innerHTML === numbers[6].innerHTML){
-//     numbers[3].classList.add("highlight");
-//     numbers[6].classList.add("highlight");
-// }
-// //Collumn 2 ----
-// if ((numbers[1].innerHTML === numbers[4].innerHTML) && (numbers[1].innerHTML === numbers[7].innerHTML)){
-//
-//     numbers[1].classList.add("highlight");
-//     numbers[4].classList.add("highlight5");
-// }
-// if (numbers[1].innerHTML === numbers[7].innerHTML){ //  +3
-//
-//     numbers[1].classList.add("highlight");
-//     numbers[7].classList.add("highlight");
-// }
-// if (numbers[4].innerHTML === numbers[7].innerHTML){
-//     numbers[4].classList.add("highlight");
-//     numbers[7].classList.add("highlight");
-// }
-// //Collumn 3 ----
-// if (numbers[2].innerHTML === numbers[5].innerHTML){
-//
-//     numbers[2].classList.add("highlight");
-//     numbers[5].classList.add("highlight");
-// }
-// if (numbers[2].innerHTML === numbers[8].innerHTML){ //  +3
-//
-//     numbers[2].classList.add("highlight");
-//     numbers[8].classList.add("highlight");
-// }
-// if (numbers[5].innerHTML === numbers[8].innerHTML){
-//     numbers[5].classList.add("highlight");
-//     numbers[8].classList.add("highlight");
-// }
-//
-// // var match = function(start){
-// //
-// // }
+var diagonalMatch = function(){
+  if(numbers[0].innerHTML === numbers[4].innerHTML){
+    numbers[0].classList.add("highlightDia");
+    numbers[4].classList.add("highlightDia");
+    document.getElementById("parent").classList.add("highlightParentDia");
+    // document.getElementById("diaTex").style.color = #f00941;
+
+  }
+  if(numbers[0].innerHTML === numbers[8].innerHTML){
+    numbers[0].classList.add("highlightDia");
+    numbers[8].classList.add("highlightDia");
+    document.getElementById("parent").classList.add("highlightParentDia");
+    // document.getElementById("diaTex").style.color = #f00941;
+  }
+  if(numbers[4].innerHTML === numbers[8].innerHTML){
+    numbers[4].classList.add("highlightDia");
+    numbers[8].classList.add("highlightDia");
+    document.getElementById("parent").classList.add("highlightParentDia");
+    // document.getElementById("diaTex").style.color = #f00941;
+  }
+};
+
+
+var diagonal2Match = function(){
+  var diaMatchCount =0;
+  if(numbers[2].innerHTML === numbers[4].innerHTML){
+    numbers[2].classList.add("highlightDia2");
+    numbers[4].classList.add("highlightDia2");
+    document.getElementById("parent").classList.add("highlightParentDia2");
+    diaMatchCount++;
+    // document.getElementById("diaText").classList.add("diaText");
+  }
+  if(numbers[2].innerHTML === numbers[6].innerHTML){
+    numbers[2].classList.add("highlightDia2");
+    numbers[6].classList.add("highlightDia2");
+    document.getElementById("parent").classList.add("highlightParentDia2");
+    diaMatchCount++;
+    // document.getElementById("diaText").classList.add("diaText");
+  }
+  if(numbers[4].innerHTML === numbers[6].innerHTML){
+    numbers[4].classList.add("highlightDia2");
+    numbers[6].classList.add("highlightDia2");
+    document.getElementById("parent").classList.add("highlightParentDia2");
+    diaMatchCount++;
+    // document.getElementById("diaText").classList.add("diaText");
+  }
+  return diaMatchCount;
+
+};
+
+var matches = diagonal2Match();
+var matches = diagonalMatch();
+
+
+
+                              //----------------\\
+//----------------------------|     /      \     |------------------
+                         //   \\----------------//
